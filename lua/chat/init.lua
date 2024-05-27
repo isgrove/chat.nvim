f611a19a6cef28dd8fca59cd16227a4bc3db0988
sam isgrove <samis@hey.com>
2024-05-27 21:50:49 +1000

    refactor: broke up code into utils file

--- lua/chat/init.lua
+++ lua/chat/init.lua
@@ -1,52 +1,27 @@
-local MAX_TOKENS = 1024 / 4
+local utils = require("chat.utils")
+
 local DEFAULT_MODEL = "gpt-4o"
 local DEFAULT_PROVIDER = "openai" -- or groq
+local MAX_TOKENS = 1024
 
 local M = {}
 
 function M.setup(opts)
-	local system_prompt = opts.system_prompt or "You are a helpful assistant."
 	local openai_api_key = opts.openai_api_key
 	local groq_api_key = opts.groq_api_key
 
-	if openai_api_key == nil and groq_api_key == nil then
-		print("Please provide either an OpenAI or Groq API key")
+	if not openai_api_key and not groq_api_key then
+		print("Please provide an API key for OpenAI, or Groq")
 		return
 	end
-
-	if openai_api_key ~= nil then
-		vim.g.openai_api_key = openai_api_key
-	end
-
-	if groq_api_key ~= nil then
-		vim.g.groq_api_key = groq_api_key
-	end
-
-	vim.g.chat_system_prompt = system_prompt
-end
-
-local function get_provider_opts(provider)
-	if provider == "openai" then
-		if vim.g.openai_api_key == nil then
-			print("Please provide an OpenAI API key in setup to use this model")
-			return
-		end
-
-		return "https://api.openai.com/v1/chat/completions", vim.g.openai_api_key
-	elseif provider == "groq" then
-		if vim.g.groq_api_key == nil then
-			print("Please provide an Groq API key in setup to use this model")
-			return
-		end
-
-		return "https://api.groq.com/openai/v1/chat/completions", vim.g.groq_api_key
-	else
-		print("Uknown API provider: " .. provider)
 		return
 	end
+	vim.g.openai_api_key = openai_api_key
+	vim.g.groq_api_key = groq_api_key
+	vim.g.chat_system_prompt = opts.system_prompt or "You are a helpful assistant."
 end
 
-local function stream_chat(messages, opts)
+local function stream_chat(content, opts)
 	local identity1 = function(chunk)
 		return chunk
 	end
@@ -54,40 +29,25 @@ local function stream_chat(messages, opts)
 	local identity = function() end
 
 	opts = opts or {}
-	local model = opts.model or DEFAULT_MODEL
-	local provider = opts.provider or DEFAULT_PROVIDER
+	opts.max_tokens = opts.max_tokens or MAX_TOKENS
+	opts.provider = opts.provider or DEFAULT_PROVIDER
+	opts.model = opts.model or DEFAULT_MODEL
+
 	local callback = opts.on_chunk or identity1
 	local on_exit = opts.on_exit or identity
 	local trim_leading = opts.trim_leading or true
-	local url, api_key = get_provider_opts(provider)
+	local url, api_key = utils.get_provider_opts(opts.provider)
 
-	local request_body = {
-		model = model,
-		messages = messages,
-		stream = true,
-	}
-
-	local request_body_json = vim.fn.json_encode(request_body)
+	local request_body_json = utils.get_request_body(content, opts)
+	local request_body_path = os.getenv("HOME") .. "/.local/share/nvim/chat_query.json"
 
 	-- NOTE: This is not ideal as we need to read/write from the disk, but this is the best way
 	-- I found to avoid new lines, quotes, backticks and etc. causing errors. From the curl man:
 	-- "When -d, --data is told to read from a file like that, carriage returns and newlines are stripped out"
 	-- TODO: Find a better method of doing this that avoids IO
-	local request_body_path = os.getenv("HOME") .. "/.local/share/nvim/chat_query.json"
-	local temp = io.open(request_body_path, "w")
-	if temp ~= nil then
-		temp:write(request_body_json)
-		temp:close()
-	end
+	utils.write_to_path(request_body_json, request_body_path)
 
-	local command = "curl --no-buffer "
-		.. url
-		.. " "
-		.. "-H 'Content-Type: application/json' -H 'Authorization: Bearer "
-		.. api_key
-		.. "' "
-		.. "-d @"
-		.. request_body_path
+	local command = utils.get_chat_command(url, api_key, request_body_path, opts.provider)
 
 	vim.g.chat_jobid = vim.fn.jobstart(command, {
 		stdout_buffered = false,
@@ -118,48 +78,6 @@ local function stream_chat(messages, opts)
 	})
 end
 
--- TODO: Find a way to undo the entire GPT response in one :undo
--- https://neovim.io/doc/user/undo.html
-local function create_response_writer(opts)
-	opts = opts or {}
-	local bufnum = vim.api.nvim_get_current_buf()
-	local line_start = opts.line_no or vim.api.nvim_buf_line_count(bufnum)
-	local nsnum = vim.api.nvim_create_namespace("gpt")
-	local extmarkid = vim.api.nvim_buf_set_extmark(bufnum, nsnum, line_start, 0, {})
-
-	local response = ""
-	return function(chunk)
-		-- Delete the currently written response
-		local num_lines = #(vim.split(response, "\n", {}))
-		vim.api.nvim_buf_set_lines(bufnum, line_start, line_start + num_lines, false, {})
-
-		-- Update the line start to wherever the extmark is now
-		line_start = vim.api.nvim_buf_get_extmark_by_id(bufnum, nsnum, extmarkid, {})[1]
-
-		-- Write out the latest
-		response = response .. chunk
-		vim.api.nvim_buf_set_lines(bufnum, line_start, line_start, false, vim.split(response, "\n", {}))
-
-		vim.cmd("undojoin")
-	end
-end
-
-local function get_visual_selection()
-	vim.cmd('noau normal! "vy"')
-	vim.cmd("noau normal! gv")
-	return vim.fn.getreg("v")
-end
-
-local function get_buffer_content()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-	return table.concat(lines, "\n")
-end
-
-local function send_keys(keys)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), "m", false)
-end
-
 function M.change_system_prompt(method)
 	local system_prompt = vim.g.chat_system_prompt
 	local opts = { prompt = "[Prompt]: ", cancelreturn = "__CANCEL__" }
@@ -184,20 +102,17 @@ function M.selection_replace(model, provider)
 		return
 	end
 
-	local content = get_visual_selection()
+	local content = utils.get_visual_selection()
 	local system_prompt = vim.g.chat_system_prompt
-	local messages = {
-		{ role = "system", content = system_prompt },
-		{ role = "user", content = content },
-	}
 
-	send_keys("d")
+	utils.send_keys("d")
 
 	if mode == "V" then
-		send_keys("O")
+		utils.send_keys("O")
 	end
 
-	stream_chat(messages, {
+	stream_chat(content, {
+		system_prompt = system_prompt,
 		model = model,
 		provider = provider,
 		trim_leading = true,
@@ -217,23 +132,19 @@ function M.completion(model, provider)
 
 	if mode == "v" or mode == "V" then
 		opts.line_no = vim.fn.line(".")
-		content = get_visual_selection()
+		content = utils.get_visual_selection()
 	else
 		local bufnum = vim.api.nvim_get_current_buf()
 		opts.line_no = vim.api.nvim_buf_line_count(bufnum)
-		content = get_buffer_content()
+		content = utils.get_buffer_content()
 	end
 
-	local messages = {
-		{ role = "system", content = system_prompt },
-		{ role = "user", content = content },
-	}
-
-	stream_chat(messages, {
+	stream_chat(content, {
+		system_prompt = system_prompt,
 		model = model,
 		provider = provider,
 		trim_leading = true,
-		on_chunk = create_response_writer(opts),
+		on_chunk = utils.create_response_writer(opts),
 	})
 end
 
---
+++ lua/chat/utils.lua
@@ -0,0 +1,108 @@
+local M = {}
+
+function M.get_provider_opts(provider)
+	local provider_data = {
+		openai = {
+			api_key = vim.g.openai_api_key,
+			name = "OpenAI",
+			url = "https://api.openai.com/v1/chat/completions",
+		},
+		groq = {
+			api_key = vim.g.groq_api_key,
+			name = "Groq",
+			url = "https://api.groq.com/openai/v1/chat/completions",
+		},
+		},
+	}
+
+	local data = provider_data[provider]
+
+	if data == nil then
+		print("Unknown API provider: " .. provider)
+		return
+	end
+
+	if data.api_key == nil then
+		print("Please provide an " .. data.name .. " API key in setup to use this model")
+		return
+	end
+
+	return data.url, data.api_key
+end
+
+function M.get_request_body(content, opts)
+	local messages = {
+		{ role = "user", content = content },
+	}
+
+	if opts.provider == "openai" or opts.provider == "groq" then
+		table.insert(messages, 1, { role = "system", content = opts.system_prompt })
+	end
+
+	local request_body = {
+		model = opts.model,
+		messages = messages,
+		stream = true,
+	}
+
+	return vim.fn.json_encode(request_body)
+end
+
+function M.write_to_path(content, path)
+	local temp = io.open(path, "w")
+	if temp ~= nil then
+		temp:write(content)
+		temp:close()
+	end
+end
+function M.get_chat_command(url, api_key, data_path, provider)
+	local command = "curl --no-buffer " .. url .. " -H 'Content-Type: application/json' "
+	if provider == "openai" or provider == "groq" then
+		command = command .. "-H 'Authorization: Bearer " .. api_key .. "' "
+	end
+
+	command = command .. "-d @" .. data_path
+
+	return command
+end
+
+-- TODO: Find a way to undo the entire GPT response in one :undo
+-- https://neovim.io/doc/user/undo.html
+function M.create_response_writer(opts)
+	opts = opts or {}
+	local bufnum = vim.api.nvim_get_current_buf()
+	local line_start = opts.line_no or vim.api.nvim_buf_line_count(bufnum)
+	local nsnum = vim.api.nvim_create_namespace("gpt")
+	local extmarkid = vim.api.nvim_buf_set_extmark(bufnum, nsnum, line_start, 0, {})
+
+	local response = ""
+	return function(chunk)
+		local num_lines = #(vim.split(response, "\n", {}))
+		vim.api.nvim_buf_set_lines(bufnum, line_start, line_start + num_lines, false, {})
+
+		line_start = vim.api.nvim_buf_get_extmark_by_id(bufnum, nsnum, extmarkid, {})[1]
+
+		response = response .. chunk
+		vim.api.nvim_buf_set_lines(bufnum, line_start, line_start, false, vim.split(response, "\n", {}))
+
+		vim.cmd("undojoin")
+	end
+end
+
+function M.get_visual_selection()
+	vim.cmd('noau normal! "vy"')
+	vim.cmd("noau normal! gv")
+	return vim.fn.getreg("v")
+end
+
+function M.get_buffer_content()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+	return table.concat(lines, "\n")
+end
+
+function M.send_keys(keys)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), "m", false)
+end
+
+return M
